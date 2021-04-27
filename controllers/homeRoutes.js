371da307588b336533f8ba78c3df56cfa5558ca5
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-04-27 00:49:41 -0400

    created the utils and some minor adjustments in the routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { BlogPost, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all blogs and JOIN with user data
+      const blogsData = await BlogPost.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+
+      // Serialize data so the template can read it
+    const blogs = blogsData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      blogs, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogsData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blog = blogsData.get({ plain: true });
+
+    res.render('project', {
+      ...blogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
