57053cd3c33dd028492dac17ba02857c73d90371
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-04-28 09:00:35 -0400

    created a new model for the commenting feature and created its relation with the other models in the index.js git commit -m

--- models/index.js
+++ models/index.js
@@ -10,4 +10,22 @@ BlogPost.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+BlogPost.hasMany(Comment, {
+  foreignKey: "blog_id",
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'blog_id'
+});
+
 module.exports = { User, BlogPost };
